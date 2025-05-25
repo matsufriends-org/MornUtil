d574410398b6d1b113611275fe386cadaa749d76
Seaeees <seaeees@gmail.com>
2025-05-25 15:48:49 +0900

    WebGLの実装を分岐

--- Cores/MornWebUtil.cs
+++ Cores/MornWebUtil.cs
@@ -6,14 +6,17 @@ namespace MornUtil
 {
     public static class MornWebUtil
     {
+#if UNITY_WEBGL
         [DllImport("__Internal")]
         private extern static void OpenWindow(string url);
-
+#endif
         public static void Open(string url)
         {
             if (Application.platform == RuntimePlatform.WebGLPlayer)
             {
+                #if UNITY_WEBGL
                 OpenWindow(url);
+                #endif
             }
             else
             {
