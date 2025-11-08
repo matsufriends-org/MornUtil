39964f6402799fb8ed38b890dbed7764726776cd
matsufriends <pikapikan0526@gmail.com>
2025-11-08 18:15:01 +0900

    ObjectPoolをMornUtilに移植

---
+++ Mono/EffectPoolMono.cs
@@ -0,0 +1,44 @@
+﻿using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+
+namespace MornUtil
+{
+    public sealed class EffectPoolMono : MonoBehaviour
+    {
+        [SerializeField] private ParticleSystem _effectPrefab;
+        [SerializeField] private int _initialSize = 3;
+        private MornObjectPool<ParticleSystem> _pool;
+
+        private void Awake()
+        {
+            _pool = new MornObjectPool<ParticleSystem>(OnGenerate, OnRent, OnReturn, _initialSize);
+        }
+
+        public void Play(Vector3 pos)
+        {
+            var effect = _pool.Rent();
+            effect.transform.position = pos;
+        }
+
+        private ParticleSystem OnGenerate()
+        {
+            var effect = Instantiate(_effectPrefab, transform);
+            var main = effect.main;
+            main.stopAction = ParticleSystemStopAction.Disable;
+            effect.OnDisableAsObservable().Subscribe(_ => _pool.Return(effect)).AddTo(this);
+            return effect;
+        }
+
+        private static void OnRent(ParticleSystem x)
+        {
+            x.gameObject.SetActive(true);
+            x.Play();
+        }
+
+        private static void OnReturn(ParticleSystem x)
+        {
+            x.Stop();
+        }
+    }
+}
